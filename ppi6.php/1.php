561f6b8f5982f4df6891ce73e009c9120beb07f1
leticia <leticia.am2007@aluno.ifsc.edu.br>
2026-06-30 23:57:40 -0300

    Correcao da 1

--- ppi6.php/1.php
+++ ppi6.php/1.php
@@ -1,31 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exemplo de Tags PHP</title>
 </head>
 <body>
 
-<h1>Resolução da questão 1  </h1>
+    <?php
+        $nomeAluno = "Carlos";
+        $nota1 = 8.5;
+        $nota2 = 9.0;
+        
+        $media = ($nota1 + $nota2) / 2;
+    ?>
 
-<li>Quais são as tags usadas para definição de código PHP? </a> </li>
-                        <ul type="disc">
-                          <li> A tag de abertura do php é   </li>
-                          <li>Alemanha → 3 faixas horizontais iguais (preto, vermelho, amarelo);</li>
-                          <li>Japão → fundo branco e círculo vermelho centralizado.</li>
-                        
-
-                        </ul>
- </ol>
-
-
-
-
-
-
-
+    <h2>Boletim Escolar</h2>
     
+    <p>Aluno: <strong><?= $nomeAluno ?></strong></p>
+    
+    <p>Média Final: <strong><?php echo $media; ?></strong></p>
+
+    <?php if ($media >= 7): ?>
+        <p style="color: green;">Situação: Aprovado!</p>
+    <?php else: ?>
+        <p style="color: red;">Situação: Reprovado.</p>
+    <?php endif; ?>
+
 </body>
 </html>
